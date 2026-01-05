d56449b4db6b4cc4fd009c377d66b4f53c456c7c
Aaron Danton <aarondanton04@hotmail.com>
2026-01-05 17:24:17 +0000

    [PROJ] refactor: don't calculate the vertex transforms when parsing the mesh
    Move node transform calculation from during the mesh parsing to when
    evaluating the meshes for animation

--- Project/src/voxelizer/parsers/assimp.cpp
+++ Project/src/voxelizer/parsers/assimp.cpp
@@ -38,6 +38,12 @@ struct Node {
     std::string name;
     std::vector<Mesh> meshes;
     std::vector<Node> nodes;
+
+    glm::mat4 transform;
+};
+
+struct ParseInformation {
+    std::unordered_map<int32_t, Material> materials;
 };
 
 void loadDiffuse(std::filesystem::path basepath, aiMaterial* material, aiTextureType type,
@@ -78,23 +84,12 @@ void loadDiffuse(std::filesystem::path basepath, aiMaterial* material, aiTexture
     }
 }
 
-Mesh processMesh(std::filesystem::path basepath, aiMesh* mesh, const aiScene* scene,
-    aiMatrix4x4 transform, std::unordered_map<int32_t, Material>& materials)
+Mesh processMesh(
+    std::filesystem::path basepath, aiMesh* mesh, const aiScene* scene, ParseInformation& parseInfo)
 {
     std::vector<glm::vec3> positions;
     std::vector<glm::vec2> uvs;
     std::vector<uint32_t> indices;
-
-    transform = transform.Transpose();
-
-    // clang-format off
-    glm::mat4 glmTransform {
-        transform.a1, transform.a2, transform.a3, transform.a4,
-        transform.b1, transform.b2, transform.b3, transform.b4,
-        transform.c1, transform.c2, transform.c3, transform.c4,
-        transform.d1, transform.d2, transform.d3, transform.d4
-    };
-    // clang-format on
 
     for (uint32_t i = 0; i < mesh->mNumVertices; i++) {
         glm::vec4 position = {
@@ -104,7 +99,7 @@ Mesh processMesh(std::filesystem::path basepath, aiMesh* mesh, const aiScene* sc
             1.f,
         };
 
-        positions.push_back(glmTransform * position * glm::vec4(1.f, -1.f, 1.f, 1.f));
+        positions.push_back(position);
 
         glm::vec2 uv(0.f);
         if (mesh->mTextureCoords[0]) {
@@ -128,7 +123,8 @@ Mesh processMesh(std::filesystem::path basepath, aiMesh* mesh, const aiScene* sc
     if (mesh->mMaterialIndex >= 0) {
         aiMaterial* material = scene->mMaterials[mesh->mMaterialIndex];
 
-        loadDiffuse(basepath, material, aiTextureType_DIFFUSE, materials, mesh->mMaterialIndex);
+        loadDiffuse(
+            basepath, material, aiTextureType_DIFFUSE, parseInfo.materials, mesh->mMaterialIndex);
         matIndex = mesh->mMaterialIndex;
     }
 
@@ -139,13 +135,12 @@ Mesh processMesh(std::filesystem::path basepath, aiMesh* mesh, const aiScene* sc
         uint32_t index = i * 3;
 
         Triangle triangle;
-        triangle.positions[0] = positions[indices[index + 0]];
-        triangle.positions[1] = positions[indices[index + 1]];
-        triangle.positions[2] = positions[indices[index + 2]];
+        for (uint32_t v = 0; v < 3; v++) {
+            uint32_t vertexIndex = indices[index + v];
+            triangle.positions[v] = positions[vertexIndex];
 
-        triangle.texture[0] = glm::vec3(uvs[indices[index + 0]], 0.f);
-        triangle.texture[1] = glm::vec3(uvs[indices[index + 1]], 0.f);
-        triangle.texture[2] = glm::vec3(uvs[indices[index + 2]], 0.f);
+            triangle.texture[v] = glm::vec3(uvs[vertexIndex], 0.f);
+        }
 
         triangle.matIndex = matIndex;
 
@@ -155,24 +150,32 @@ Mesh processMesh(std::filesystem::path basepath, aiMesh* mesh, const aiScene* sc
     return returnMesh;
 }
 
-Node parseAssimpNode(std::filesystem::path basepath, aiNode* node, const aiScene* scene,
-    aiMatrix4x4 transform, std::unordered_map<int32_t, Material>& materials)
+Node parseAssimpNode(
+    std::filesystem::path basepath, aiNode* node, const aiScene* scene, ParseInformation& parseInfo)
 {
     Node returnNode;
     std::vector<Triangle> triangles;
     returnNode.name = node->mName.C_Str();
 
+    // clang-format off
+    aiMatrix4x4 transform = node->mTransformation;
+    returnNode.transform = glm::mat4(
+            transform.a1, transform.b1, transform.c1, transform.d1,
+            transform.a2, transform.b2, transform.c2, transform.d2,
+            transform.a3, transform.b3, transform.c3, transform.d3,
+            transform.a4, transform.b4, transform.c4, transform.d4
+    );
+    // clang-format on
+
     for (uint32_t i = 0; i < node->mNumMeshes; i++) {
         aiMesh* mesh = scene->mMeshes[node->mMeshes[i]];
-        Mesh newMesh = processMesh(basepath, mesh, scene, transform, materials);
+        Mesh newMesh = processMesh(basepath, mesh, scene, parseInfo);
 
         returnNode.meshes.push_back(newMesh);
     }
 
     for (uint32_t i = 0; i < node->mNumChildren; i++) {
-        aiMatrix4x4 childTransform = transform * node->mChildren[i]->mTransformation;
-        Node newNode
-            = parseAssimpNode(basepath, node->mChildren[i], scene, childTransform, materials);
+        Node newNode = parseAssimpNode(basepath, node->mChildren[i], scene, parseInfo);
 
         returnNode.nodes.push_back(newNode);
     }
@@ -206,7 +209,7 @@ std::vector<Animation> parseAnimations(const aiScene* scene)
 
                 channel.positions.push_back({
                     pos.mTime,
-                    { pos.mValue.x, -pos.mValue.y, pos.mValue.z },
+                    { pos.mValue.x, pos.mValue.y, pos.mValue.z },
                 });
             }
 
@@ -237,6 +240,23 @@ std::vector<Animation> parseAnimations(const aiScene* scene)
     return animations;
 }
 
+template <typename Type>
+glm::mat4 calculateLerp(const std::vector<std::pair<double, Type>>& channel, float target,
+    std::function<glm::mat4(Type a, Type b, float t)> lerpFunction)
+{
+    if (channel.size() != 0) {
+        for (size_t i = 0; i < channel.size() - 1; i++) {
+            if (target >= channel[i].first && target < channel[i + 1].first) {
+                float t = (target - channel[i].first) / (channel[i + 1].first - channel[i].first);
+
+                return lerpFunction(channel[i].second, channel[i + 1].second, t);
+            }
+        }
+    }
+
+    return glm::mat4(1.f);
+}
+
 glm::mat4 evaluateTransform(
     std::string node, const Animation& animation, float time, glm::mat4 transform)
 {
@@ -244,47 +264,25 @@ glm::mat4 evaluateTransform(
         if (channel.target != node)
             continue;
 
-        if (channel.scales.size() != 0) {
-            for (size_t i = 0; i < channel.scales.size() - 1; i++) {
-                if (time >= channel.scales[i].first && time < channel.scales[i + 1].first) {
-                    float t = (time - channel.scales[i].first)
-                        / (channel.scales[i + 1].first - channel.scales[i].first);
+        glm::mat4 scale
+            = calculateLerp<glm::vec3>(channel.scales, time, [](glm::vec3 a, glm::vec3 b, float t) {
+                  glm::vec3 lerp = (1.f - t) * a + t * b;
+                  return glm::scale(glm::mat4(1.f), lerp);
+              });
 
-                    glm::vec3 lerp
-                        = (1.f - t) * channel.scales[i].second + t * channel.scales[i + 1].second;
+        glm::mat4 rotation = calculateLerp<glm::quat>(
+            channel.rotations, time, [](glm::quat a, glm::quat b, float t) {
+                glm::quat lerp = glm::lerp(a, b, t);
+                return glm::mat4_cast(glm::normalize(lerp));
+            });
 
-                    transform = glm::scale(transform, lerp);
-                }
-            }
-        }
+        glm::mat4 translate = calculateLerp<glm::vec3>(
+            channel.positions, time, [](glm::vec3 a, glm::vec3 b, float t) {
+                glm::vec3 lerp = (1.f - t) * a + t * b;
+                return glm::translate(glm::mat4(1.f), lerp);
+            });
 
-        if (channel.rotations.size() != 0) {
-            for (size_t i = 0; i < channel.rotations.size() - 1; i++) {
-                if (time >= channel.rotations[i].first && time < channel.rotations[i + 1].first) {
-                    float t = (time - channel.rotations[i].first)
-                        / (channel.rotations[i + 1].first - channel.rotations[i].first);
-
-                    glm::quat lerp = glm::lerp(
-                        channel.rotations[i].second, channel.rotations[i + 1].second, t);
-
-                    transform = transform * glm::mat4_cast(glm::normalize(lerp));
-                }
-            }
-        }
-
-        if (channel.positions.size() != 0) {
-            for (size_t i = 0; i < channel.positions.size() - 1; i++) {
-                if (time >= channel.positions[i].first && time < channel.positions[i + 1].first) {
-                    float t = (time - channel.positions[i].first)
-                        / (channel.positions[i + 1].first - channel.positions[i].first);
-
-                    glm::vec3 lerp = (1.f - t) * channel.positions[i].second
-                        + t * channel.positions[i + 1].second;
-
-                    transform = glm::translate(transform, lerp);
-                }
-            }
-        }
+        transform = transform * translate * rotation * scale;
     }
 
     return transform;
@@ -297,14 +295,24 @@ std::vector<Triangle> evaluateNodes(
 
     transform = evaluateTransform(node.name, animation, time, transform);
 
+    // clang-format off
+    const glm::mat4 triangleTransform = {
+        1.f,  0.f, 0.f, 0.f,
+        0.f, -1.f, 0.f, 0.f,
+        0.f,  0.f, 1.f, 0.f,
+        0.f,  0.f, 0.f, 1.f
+    };
+    // clang-format on
+
     for (const auto& mesh : node.meshes) {
         for (size_t i = 0; i < mesh.triangles.size(); i++) {
-            triangles.push_back(transformTriangle(mesh.triangles[i], transform));
+            triangles.push_back(transformTriangle(
+                transformTriangle(mesh.triangles[i], transform), triangleTransform));
         }
     }
 
     for (const auto& node : node.nodes) {
-        auto nodeTriangles = evaluateNodes(node, animation, time, transform);
+        auto nodeTriangles = evaluateNodes(node, animation, time, transform * node.transform);
         triangles.insert(triangles.end(), nodeTriangles.begin(), nodeTriangles.end());
     }
 
@@ -313,7 +321,7 @@ std::vector<Triangle> evaluateNodes(
 
 std::vector<Triangle> evaluateNodes(const Node& node)
 {
-    return evaluateNodes(node, Animation {}, 0.0f, glm::mat4(1.f));
+    return evaluateNodes(node, Animation {}, 0.0f, node.transform);
 }
 
 ParserRet parseAssimp(std::filesystem::path path, const ParserArgs& args)
@@ -329,12 +337,11 @@ ParserRet parseAssimp(std::filesystem::path path, const ParserArgs& args)
         exit(-1);
     }
 
-    std::unordered_map<int32_t, Material> materials;
+    ParseInformation parseInfo;
 
     std::vector<Animation> animations = parseAnimations(scene);
 
-    Node baseNode = parseAssimpNode(
-        path.parent_path(), scene->mRootNode, scene, scene->mRootNode->mTransformation, materials);
+    Node baseNode = parseAssimpNode(path.parent_path(), scene->mRootNode, scene, parseInfo);
 
     glm::uvec3 dimensions;
     std::vector<std::unordered_map<glm::ivec3, glm::vec3>> baseVoxels;
@@ -353,7 +360,7 @@ ParserRet parseAssimp(std::filesystem::path path, const ParserArgs& args)
         meshes.push_back(evaluateNodes(baseNode));
     }
 
-    std::tie(dimensions, baseVoxels) = parseMeshes(meshes, materials, args);
+    std::tie(dimensions, baseVoxels) = parseMeshes(meshes, parseInfo.materials, args);
 
     return { dimensions, baseVoxels };
 }
